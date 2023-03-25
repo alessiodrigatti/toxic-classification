929a3da6f252bb89e060b355093f29d01a90d734
Alessio Drigatti <drigaale@students.zhaw.ch>
2023-03-25 14:36:31 +0100

    smaler sample size

--- sampling_df.py
+++ sampling_df.py
@@ -3,9 +3,9 @@ import pandas as pd
 def sampling():
 
     df = pd.read_json("data_pp.json", orient="columns")
-    df_sample = df.sample(frac=0.25, ignore_index=True)
+    df_sample = df.sample(frac=0.10, ignore_index=True)
     df_sample.reset_index(drop=True, inplace=True)
-    df_sample.to_json("data_pp_sample.json")
+    df_sample.to_json("data_pp_sample2.json")
 
 
 sampling()
